a67bf86657c0a0d1175e563ceb89009f391357ce
camelcrush <dkxm0927@naver.com>
2022-09-25 20:01:12 +0900

    Init & Apollo Install

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { ApolloDriver } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [GraphQLModule.forRoot({ driver: ApolloDriver })],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
