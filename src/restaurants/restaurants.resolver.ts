c9d7ba3c7c4e4e37313b085155981b18612194b4
camelcrush <dkxm0927@naver.com>
2022-09-26 20:32:28 +0900

    InputType & ArgsType : Mutation

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,4 +1,5 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver((of) => Restaurant)
@@ -6,5 +7,11 @@ export class RestaurantResolver {
   @Query((returns) => [Restaurant])
   isPizzaGood(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
     return [];
+  }
+
+  @Mutation((returns) => Boolean)
+  createRestaurant(@Args() createRestaurant: CreateRestaurantDto): boolean {
+    console.log(createRestaurant);
+    return true;
   }
 }
