45ba087841f2734b1508ade504034dba854e0dc6
camelcrush <dkxm0927@naver.com>
2022-10-03 19:54:16 +0900

    Login 2

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -20,13 +20,7 @@ export class UsersResolver {
     @Args('input') createAccountInput: CreateAccountInput,
   ): Promise<CreateAccountOutput> {
     try {
-      const { ok, error } = await this.usersService.createAccount(
-        createAccountInput,
-      );
-      return {
-        ok,
-        error,
-      };
+      return this.usersService.createAccount(createAccountInput);
     } catch (error) {
       return {
         ok: false,
@@ -37,6 +31,13 @@ export class UsersResolver {
 
   @Mutation((returns) => LoginOutput)
   async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
-    return;
+    try {
+      return this.usersService.login(loginInput);
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dtos/create-account.dto';
+import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -25,6 +26,32 @@ export class UsersService {
     } catch (e) {
       // make error
       return { ok: false, error: "Couldn't create account" };
+    }
+  }
+
+  async login({
+    email,
+    password,
+  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+    try {
+      const user = await this.users.findOne({ where: { email } });
+      if (!user) {
+        return { ok: false, error: 'User not found' };
+      }
+      const passwordOk = user.checkPassword(password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: 'Wrong Password',
+        };
+      }
+      return {
+        ok: true,
+        token: 'alalalalalal',
+      };
+    } catch (error) {
+      console.log(error);
+      return { ok: false, error };
     }
   }
 }
