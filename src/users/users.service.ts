7aa60976f4448d5b1a3233eaa82eb1f20c8e7234
camelcrush <dkxm0927@naver.com>
2022-10-14 22:03:34 +0900

    verifyEmail Test

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -131,7 +131,7 @@ export class UsersService {
       }
       return { ok: false, error: 'Verification not found.' };
     } catch (error) {
-      return { ok: false, error };
+      return { ok: false, error: 'Coult not verify email' };
     }
   }
 }
