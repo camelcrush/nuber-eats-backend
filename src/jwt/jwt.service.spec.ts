9773509b20c82a9dc830d6c26ae2a841f1f8f4e2
camelcrush <dkxm0927@naver.com>
2022-10-14 23:25:18 +0900

    JWT Sign Test

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -1,6 +1,13 @@
 import { Test } from '@nestjs/testing';
+import * as jwt from 'jsonwebtoken';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { JwtService } from './jwt.service';
+
+jest.mock('jsonwebtoken', () => {
+  return {
+    sign: jest.fn(() => 'TOKEN'),
+  };
+});
 
 const TEST_KEY = 'testKey';
 
@@ -8,13 +15,26 @@ describe('JwtService', () => {
   let service: JwtService;
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [JwtService, { provide: CONFIG_OPTIONS, useValue: TEST_KEY }],
+      providers: [
+        JwtService,
+        { provide: CONFIG_OPTIONS, useValue: { privateKey: TEST_KEY } },
+      ],
     }).compile();
     service = module.get<JwtService>(JwtService);
   });
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-  it.todo('sign');
+
+  describe('sign', () => {
+    it('should return a signed token', () => {
+      const ID = 1;
+      const token = service.sign(ID);
+      expect(typeof token).toBe('string');
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenCalledWith({ id: ID }, TEST_KEY);
+    });
+  });
+
   it.todo('verify');
 });
