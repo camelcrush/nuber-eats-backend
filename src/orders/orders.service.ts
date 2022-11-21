38a76aa56a2befcbc736d53f19d29b8ac5b0c704
camelcrush <dkxm0927@naver.com>
2022-11-22 00:03:27 +0900

    orderUpdates

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { PubSub } from 'graphql-subscriptions';
 import {
   NEW_COOKED_ORDER,
+  NEW_ORDER_UPDATE,
   NEW_PENDING_ORDER,
   PUB_SUB,
 } from 'src/common/common.constants';
@@ -233,14 +234,16 @@ export class OrderService {
           error: 'Not authorized',
         };
       }
+      const newOrder = { ...order, status };
       await this.orders.save({ id: orderId, status });
       if (user.role === UserRole.Owner) {
         if (status === OrderStatus.Cooked) {
           await this.pubSub.publish(NEW_COOKED_ORDER, {
-            cookedOrders: { ...order, status },
+            cookedOrders: newOrder,
           });
         }
       }
+      await this.pubSub.publish(NEW_ORDER_UPDATE, { orderUpdates: newOrder });
       return {
         ok: true,
       };
