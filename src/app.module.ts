6ab35f498bc7723df051f1772ca9c5773f927515
camelcrush <dkxm0927@naver.com>
2022-11-24 22:59:16 +0900

    Task Scheduling

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,6 +26,7 @@ import { OrderItem } from './orders/entities/order-item.entity';
 import { CommonModule } from './common/common.module';
 import { PaymentsModule } from './payments/payments.module';
 import { Payment } from './payments/entities/payment.entity';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
@@ -80,6 +81,7 @@ import { Payment } from './payments/entities/payment.entity';
       autoSchemaFile: true,
       context: ({ req, connection }) => ({ token: req.headers['x-jwt'] }),
     }),
+    ScheduleModule.forRoot(),
     JwtModule.forRoot({
       privateKey: process.env.PRIVATE_KEY,
     }),
